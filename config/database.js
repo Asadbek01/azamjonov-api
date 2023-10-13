9f7a09508727e775c36bfa6309a1c8c3332431c1
Asadbek01 <asadbek.azamjonov@devkom.pl>
2023-10-13 23:23:09 +0200

    done

--- config/database.js
+++ config/database.js
@@ -1,22 +1,29 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
+  const client = env('DATABASE_CLIENT', 'postgres'); // Change the default client to 'postgres'
 
-  const connections = {
+  const connections = {    
+    // Keep the MySQL configuration as is
+    
     postgres: {
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'blogs'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'Enjoy_l1fe'),
+        host: env('DATABASE_HOST', '127.0.0.1'), // Set the hostname or IP address of your PostgreSQL server
+        port: env.int('DATABASE_PORT', 5432), // Set the port your PostgreSQL server is running on (5432 is the default)
+        database: env('DATABASE_NAME', 'blogs'), // Set your PostgreSQL database name
+        user: env('DATABASE_USERNAME', 'postgres'), // Set your PostgreSQL username
+        password: env('DATABASE_PASSWORD', 'Enjoy_l1fe'), // Set your PostgreSQL password
+        ssl: env.bool('DATABASE_SSL', false) && {
+          key: env('DATABASE_SSL_KEY', undefined),
+          cert: env('DATABASE_SSL_CERT', undefined),
+          ca: env('DATABASE_SSL_CA', undefined),
+          capath: env('DATABASE_SSL_CAPATH', undefined),
+          cipher: env('DATABASE_SSL_CIPHER', undefined),
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: {
-        min: env.int('DATABASE_POOL_MIN', 2),
-        max: env.int('DATABASE_POOL_MAX', 10),
-      },
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
   };
 
