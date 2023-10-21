07efc5545142d95761e10e11f452b57702f08eab
Asadbek_Azamjonov <asadbek.azamjonov@devkom.pl>
2023-10-21 04:29:04 +0200

    added findOne route

--- src/api/blog/controllers/blog.js
+++ src/api/blog/controllers/blog.js
@@ -6,4 +6,19 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::blog.blog');
+module.exports = createCoreController('api::blog.blog', ({ strapi }) => ({
+    async findOne(ctx) {
+        const { id } = ctx.params;
+
+        const entity = await strapi.db.query('api::blog.blog').findOne({ 
+            where: { slug: id },    
+            populate: ['thumbnail'],
+         });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+
+        },
+
+}));
+
